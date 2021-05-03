HRS normalization bugfix

methods/fdm/operations/gradient_hrs.py
@@ -141,6 +141,7 @@ class GradientHRS(Operation):
         xd = self.x[i_d]
 
         phi_p = self.normalize(flux_p, flux_u, flux_d)
+
         tetha_p = self.normalize(xp, xu, xd)
         if direction[0] >= 0:
             tetha_p[0] = tetha_p[1]
@@ -161,8 +162,9 @@ class GradientHRS(Operation):
 
     @staticmethod
     def normalize(p, u, d):
-        eps = 1e-9
+        num = p - u
         den = d - u
-        den[den == 0] = eps
-        #den[np.absolute(den) < eps] = eps
-        return (p - u) / den
+        check = den != 0
+        out = np.ones_like(p)
+        out[check] = num[check] / den[check]
+        return out

tests/solvers/test_dassl_solver_burgers.py
@@ -43,10 +43,12 @@ class TestDasslSolverBurgers(TestCase):
         x = np.linspace(0, 1., N)
         y0 = +(np.sin(2*3.1415*x) + np.sin(3.1415*x)/2)
 
+        #lb = ModelParameter("lb", 0.0, "m/s")
+        #ub = ModelParameter("ub", 0.0, "m/s")
         lb = ModelParameter("lb", None, "m/s")
         ub = ModelParameter("ub", None, "m/s")
 
-        model = Burgers(x, lb=lb, ub=ub, scheme=SchemeM1FDMEnum.CENTRAL_N4, flux_delimiter=FluxDelimiterEnum.CUBISTA2)
+        model = Burgers(x, lb=lb, ub=ub, scheme=SchemeM1FDMEnum.CENTRAL_N4, flux_delimiter=FluxDelimiterEnum.SMART2)
 
         return t0, y0, x, model
 
@@ -65,7 +67,7 @@ class TestDasslSolverBurgers(TestCase):
 
         t0, y0, x, model = self.case1()
 
-        t, y, yp = DasslSolver.run(model, t0, y0, display=True, rtol=1e-3, atol=1e-3)
+        t, y, yp = DasslSolver.run(model, t0, y0, display=True, rtol=1e-6, atol=1e-6)
 
         if plot:
             self.plot_result(t, x, y)
@@ -77,7 +79,7 @@ class TestDasslSolverBurgers(TestCase):
 
         t0, y0, x, model = self.case2()
 
-        t, y, yp = DasslSolver.run(model, t0, y0, display=True, rtol=1e-6, atol=1e-6)
+        t, y, yp = DasslSolver.run(model, t0, y0, display=True, rtol=1e-3, atol=1e-6)
 
         if plot:
             self.plot_result(t, x, y)